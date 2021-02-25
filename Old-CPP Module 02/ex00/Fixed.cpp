c87935cf8c38e93542c9e1c94a1ca11c4398d5d7
Sithi5 <ma.sithis@gmail.com>
2021-02-25 12:10:36 +0100

    push ex00

---
+++ Old-CPP Module 02/ex00/Fixed.cpp
@@ -0,0 +1,39 @@
+//
+// Created by MaSit on 25/02/2021.
+//
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void): _value(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void) {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &copy){
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+
+    return;
+}
+
+Fixed &Fixed::operator=(const Fixed &rhs) {
+    std::cout << "Assignation operator called" << std::endl;
+    if (this != &rhs){
+        this->setRawBits(rhs.getRawBits());
+    }
+    return *this;
+}
+
+int Fixed::getRawBits() const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_value;
+}
+
+void Fixed::setRawBits(const int raw) {
+    this->_value = raw;
+}
+
+
---
+++ Old-CPP Module 02/ex00/Fixed.hpp
@@ -0,0 +1,29 @@
+//
+// Created by MaSit on 25/02/2021.
+//
+
+#ifndef OLD_CPP_MODULE_FIXED_HPP
+#define OLD_CPP_MODULE_FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+public:
+    Fixed(void);
+    virtual ~Fixed(void);
+
+    Fixed(Fixed const &copy);
+    Fixed & operator=(Fixed const & rhs);
+
+    int getRawBits() const;
+    void setRawBits(int const raw);
+
+private:
+    int _value;
+    int const static _rawBits = 8;
+
+
+};
+
+
+#endif //OLD_CPP_MODULE_FIXED_HPP
