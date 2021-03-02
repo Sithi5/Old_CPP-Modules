ccd1a8637ff543e7f78185ef4c3803fceb576b5b
maginist <maginist@student.42.fr>
2021-03-02 16:26:32 +0100

    modif main

--- Old-CPP Module 04/ex01/main.cpp
+++ Old-CPP Module 04/ex01/main.cpp
@@ -12,22 +12,44 @@ int main()
 
 	std::cout << *moi;
 
-	Enemy* b = new RadScorpion();
+	Enemy* rs = new RadScorpion();
+    std::cout << rs->getType() << " has now " << rs->getHP() << " HP" << std::endl;
+	Enemy* sm = new SuperMutant();
 
 	AWeapon* pr = new PlasmaRifle();
 	AWeapon* pf = new PowerFist();
 
+    std::cout << sm->getType() << " has now " << sm->getHP() << " HP" << std::endl << std::endl;
 	moi->equip(pr);
 	std::cout << *moi;
 	moi->equip(pf);
 
-	moi->attack(b);
+	moi->attack(rs);
 	std::cout << *moi;
 	moi->equip(pr);
 	std::cout << *moi;
-	moi->attack(b);
+	moi->attack(rs);
+    std::cout << rs->getType() << " has now " << rs->getHP() << " HP" << std::endl << std::endl;
 	std::cout << *moi;
-	moi->attack(b);
-	std::cout << *moi;
+	moi->attack(sm);
+    std::cout << sm->getType() << " has now " << sm->getHP() << " HP" << std::endl << std::endl;
+    for (int i=0; sm->getHP() > 50; i++)
+    {
+        moi->attack(sm);
+        std::cout << *moi;
+        if (moi->getAP() < 10)
+        {
+            std::cout << std::endl << "I need some Nuka-Cola Quantum! *pssst*" << std::endl;
+            moi->recoverAP();
+            std::cout << *moi <<std::endl;
+        }
+    }
+    std::cout << sm->getType() << " has now " << sm->getHP() << " HP" << std::endl;
+    moi->equip(pf);
+    moi->attack(sm);
+    std::cout << *moi;
+
+
+
 	return 0;
 }
