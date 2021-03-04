21c609574394aba95d71c9d32c75c0871eba13e8
Sithi5 <ma.sithis@gmail.com>
2021-03-04 14:28:03 +0100

    fix ex02

--- Old-CPP Module 04/ex02/AssaultTerminator.cpp
+++ Old-CPP Module 04/ex02/AssaultTerminator.cpp
@@ -1,36 +1,36 @@
 #include "AssaultTerminator.hpp"
 
 AssaultTerminator::AssaultTerminator(void) {
-	std::cout << "* teleports from space *" << std::endl;
+    std::cout << "* teleports from space *" << std::endl;
 }
 
 AssaultTerminator::AssaultTerminator(AssaultTerminator const &copy) {
-	*this = copy;
+    *this = copy;
 }
 
 AssaultTerminator::~AssaultTerminator(void) {
-	std::cout <<  "I’ll be back ..." << std::endl;
+    std::cout << "I'll be back ..." << std::endl;
 }
 
-AssaultTerminator & AssaultTerminator::operator=(AssaultTerminator const &rhs)
-{
-	(void)rhs;
-	return *this;
+AssaultTerminator &AssaultTerminator::operator=(AssaultTerminator const &rhs) {
+    (void) rhs;
+    return *this;
 }
 
-ISpaceMarine* AssaultTerminator::clone() const {
-	return (new AssaultTerminator(*this));
+ISpaceMarine *AssaultTerminator::clone() const {
+    return (new AssaultTerminator(*this));
 }
 
 void AssaultTerminator::battleCry() const {
-	std::cout << "This code is unclean. Purify it !" << std::endl;
+    std::cout << "This code is unclean. Purify it !" << std::endl;
 }
 
 void AssaultTerminator::rangedAttack() const {
-	std::cout << "* does nothing *" << std::endl;
+    std::cout << "* does nothing *" << std::endl;
 }
+
 void AssaultTerminator::meleeAttack() const {
-	std::cout << "* attaque with chainfists *" << std::endl;
+    std::cout << "* attaque with chainfists *" << std::endl;
 }
 
 const std::string &AssaultTerminator::getName() const {
