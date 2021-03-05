4a16e4d563ce474481311b0148b9abffabfcdec0
ChokMania <judumay@student.42.fr>
2021-03-05 11:29:45 +0100

    Working CPP00

--- Old-CPP Module 00/ex01/contact.cpp
+++ Old-CPP Module 00/ex01/contact.cpp
@@ -12,17 +12,17 @@ void Contact::setContact(std::string first_name, std::string last_name, std::str
 						 std::string email_address, std::string phone_number, std::string birthday_date, std::string favorite_meal,
 						 std::string underwear_color, std::string darkest_secret)
 {
-	this->_first_name = first_name;
-	this->_last_name = last_name;
-	this->_nickname = nickname;
-	this->_login = login;
-	this->_postal_address = postal_address;
-	this->_email_address = email_address;
-	this->_phone_number = phone_number;
-	this->_birthday_date = birthday_date;
-	this->_favorite_meal = favorite_meal;
-	this->_underwear_color = underwear_color;
-	this->_darkest_secret = darkest_secret;
+	setFirstName(first_name);
+	setLastName(last_name);
+	setNickname(nickname);
+	setLogin(login);
+	setPostalAddress(postal_address);
+	setEmailAddress(email_address);
+	setPhoneNumber(phone_number);
+	setBirthdayDate(birthday_date);
+	setFavoriteMeal(favorite_meal);
+	setUnderwearColor(underwear_color);
+	setDarkestSecret(darkest_secret);
 	this->_set = 1;
 }
 
@@ -30,27 +30,24 @@ void Contact::display()
 {
 	if (this->isSet())
 	{
-		std::cout << "first_name = " << this->_first_name << std::endl;
-		std::cout << "last_name = " << this->_last_name << std::endl;
-		std::cout << "login = " << this->_login << std::endl;
-		std::cout << "postal_address = " << this->_postal_address << std::endl;
-		std::cout << "email_address = " << this->_email_address << std::endl;
-		std::cout << "phone_number = " << this->_phone_number << std::endl;
-		std::cout << "birthday_date = " << this->_birthday_date << std::endl;
-		std::cout << "favorite_meal = " << this->_favorite_meal << std::endl;
-		std::cout << "underwear_color = " << this->_underwear_color << std::endl;
-		std::cout << "darkest_secret = " << this->_darkest_secret << std::endl;
+		std::cout << "first_name = " << getFirstName() << std::endl;
+		std::cout << "last_name = " << getLastName() << std::endl;
+		std::cout << "login = " << getLogin() << std::endl;
+		std::cout << "postal_address = " << getPostalAddress() << std::endl;
+		std::cout << "email_address = " << getEmailAddress() << std::endl;
+		std::cout << "phone_number = " << getPhoneNumber() << std::endl;
+		std::cout << "birthday_date = " << getBirthdayDate() << std::endl;
+		std::cout << "favorite_meal = " << getFavoriteMeal() << std::endl;
+		std::cout << "underwear_color = " << getUnderwearColor() << std::endl;
+		std::cout << "darkest_secret = " << getDarkestSecret() << std::endl;
 	}
 }
 
-bool Contact::isSet()
-{
-	return this->_set;
-}
+bool Contact::isSet() { return this->_set; }
 
 void Contact::setFirstName(std::string first_name) { this->_first_name = first_name; }
 void Contact::setLastName(std::string last_name) { this->_last_name = last_name; }
-void Contact::setNickname(std::string _nickname) { this->_first_name = _nickname; }
+void Contact::setNickname(std::string _nickname) { this->_nickname = _nickname; }
 void Contact::setLogin(std::string login) { this->_login = login; }
 void Contact::setPostalAddress(std::string postal_address) { this->_postal_address = postal_address; }
 void Contact::setEmailAddress(std::string email_address) { this->_email_address = email_address; }
@@ -60,20 +57,14 @@ void Contact::setFavoriteMeal(std::string favorite_meal) { this->_favorite_meal 
 void Contact::setUnderwearColor(std::string underwear_color) { this->_underwear_color = underwear_color; }
 void Contact::setDarkestSecret(std::string darkest_secret) { this->_darkest_secret = darkest_secret; }
 
-std::string Contact::getFirstName()
-{
-	return (this->_first_name);
-}
-
+std::string Contact::getFirstName() { return (this->_first_name); }
 std::string Contact::getLastName() { return (this->_last_name); }
 std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
-std::string Contact::getNickname() { return (this->_nickname); }
+std::string Contact::getLogin() { return (this->_login); }
+std::string Contact::getPostalAddress() { return (this->_postal_address); }
+std::string Contact::getEmailAddress() { return (this->_email_address); }
+std::string Contact::getPhoneNumber() { return (this->_phone_number); }
+std::string Contact::getBirthdayDate() { return (this->_birthday_date); }
+std::string Contact::getFavoriteMeal() { return (this->_favorite_meal); }
+std::string Contact::getUnderwearColor() { return (this->_underwear_color); }
+std::string Contact::getDarkestSecret() { return (this->_darkest_secret); }
--- Old-CPP Module 00/ex01/main.cpp
+++ Old-CPP Module 00/ex01/main.cpp
@@ -2,122 +2,137 @@
 #include "contact.hpp"
 #include "sstream"
 
-void print_with_width(std::string str, int width){
-    std::cout.width(width);
-    std::cout << str;
-    return ;
+void print_with_width(std::string str, int width)
+{
+	std::cout.width(width);
+	std::cout << str;
+	return;
 }
 
-void print_formated_contact_info(std::string str){
-    if (str.size() >= 10){
-        str.resize(9);
-        str.append(".");
-    }
-    str.append("|");
-    print_with_width(str, 11);
+void print_formated_contact_info(std::string str)
+{
+	if (str.size() >= 10)
+	{
+		str.resize(9);
+		str.append(".");
+	}
+	str.append("|");
+	print_with_width(str, 11);
 }
 
-int main(void){
-    contact phone_book[8];
-    std::string input;
+int main(void)
+{
+	Contact phone_book[8];
+	std::string input;
 
-    int     i = 0;
+	int i = 0;
 
-    while (1){
-        std::cout << "Waiting for commands : ['EXIT', 'SEARCH', 'ADD']." << std::endl;
-        std::cout << "input: ";
+	while (1)
+	{
+		std::cout << "Waiting for commands : ['EXIT', 'SEARCH', 'ADD']." << std::endl;
+		std::cout << "input: ";
 
-        std::getline(std::cin ,input);
-        if (input == "EXIT"){
-            break;
-        }
-        else if (input == "ADD"){
-            if (phone_book[7].isSet()){
-                std::cout << "Your phonebook is full !" << std::endl;
-                continue;
-            }
+		std::getline(std::cin, input);
+		if (input == "EXIT")
+		{
+			break;
+		}
+		else if (input == "ADD")
+		{
+			if (phone_book[7].isSet())
+			{
+				std::cout << "Your phonebook is full !" << std::endl;
+				continue;
+			}
 
-            std::string input_string[11] = {
-                    "first name",
-                    "last name",
-                    "nickname",
-                    "login",
-                    "postal_address",
-                    "email_address",
-                    "phone_number",
-                    "birthday_date",
-                    "favorite_meal",
-                    "underwear_color",
-                    "darkest_secret"
-            };
-            std::string input[11];
+			std::string input_string[11] = {
+				"first name",
+				"last name",
+				"nickname",
+				"login",
+				"postal_address",
+				"email_address",
+				"phone_number",
+				"birthday_date",
+				"favorite_meal",
+				"underwear_color",
+				"darkest_secret"};
+			std::string input[11];
 
-            for (int i = 0 ; i < 11; ++i){
-                  std::cout << input_string[i] << ": " << std::endl;
-                  std::cout << "input: ";
-                  std::getline(std::cin, input[i]);
-            }
+			for (int i = 0; i < 11; ++i)
+			{
+				std::cout << input_string[i] << ": " << std::endl;
+				std::cout << "input: ";
+				std::getline(std::cin, input[i]);
+			}
 
-            phone_book[i].setContact(input[0], input[1], input[2], input[3], input[4], input[5], input[6], input[7], input[8], input[9], input[10]);
-            std::cout << "Contact " << input[0] << " has been created with the following info : " << std::endl;
-            phone_book[i++].display();
-        }
-        else if (input == "SEARCH"){
-            int research_id = -1;
-            std::string display_first_name;
-            std::string display_last_name;
-            std::string display_nickname;
-            std::string display_index;
-            print_with_width("id|", 11);
-            print_with_width("firstname|", 11);
-            print_with_width("lastname|", 11);
-            print_with_width("nickname|", 11);
-            std::cout << std::endl;
+			phone_book[i].setContact(input[0], input[1], input[2], input[3], input[4], input[5], input[6], input[7], input[8], input[9], input[10]);
+			std::cout << "Contact " << input[0] << " has been created with the following info : " << std::endl;
+			phone_book[i++].display();
+		}
+		else if (input == "SEARCH")
+		{
+			int research_id = -1;
+			std::string display_first_name;
+			std::string display_last_name;
+			std::string display_nickname;
+			std::string display_index;
+			print_with_width("id|", 11);
+			print_with_width("firstname|", 11);
+			print_with_width("lastname|", 11);
+			print_with_width("nickname|", 11);
+			std::cout << std::endl;
 
-            for (int j = 0; j < 8; j++) {
-                if (phone_book[j].isSet()) {
-                    display_first_name = phone_book[j].getFirstName();
-                    display_last_name = phone_book[j].getLastName();
-                    display_nickname = phone_book[j].getNickname();
-                    std::cout.width(10);
-                    std::cout << j << "|";
-                    print_formated_contact_info(display_first_name);
-                    print_formated_contact_info(display_last_name);
-                    print_formated_contact_info(display_nickname);
-                    std::cout << std::endl;
-                }
-            }
+			for (int j = 0; j < 8; j++)
+			{
+				if (phone_book[j].isSet())
+				{
+					display_first_name = phone_book[j].getFirstName();
+					display_last_name = phone_book[j].getLastName();
+					display_nickname = phone_book[j].getNickname();
+					std::cout.width(10);
+					std::cout << j << "|";
+					print_formated_contact_info(display_first_name);
+					print_formated_contact_info(display_last_name);
+					print_formated_contact_info(display_nickname);
+					std::cout << std::endl;
+				}
+			}
 
-            std::cout << "Which contact do you want to display ? " << std::endl;
-            std::getline(std::cin, input);
-            std::stringstream ss(input);
-            if ((ss >> research_id).fail()){
-                research_id = -1;
-            }
-            std::cout << "id = " << research_id <<std::endl;
-            if (research_id > 7 || research_id < 0 || !phone_book[research_id].isSet()){
-                std::cout << "Invalid Id." << std::endl;
-            }
-            else{
-                std::cout << "selected id : " << research_id<< std::endl;
-                phone_book[research_id].display();
-            }
-        }
-//        else if (input == "FILL"){
-//            phone_book[i++].setContact("tititititi", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//            phone_book[i++].setContact("ratatatatatatatatatatatatatata", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//            phone_book[i++].setContact("toto", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//            phone_book[i++].setContact("MINET", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//            phone_book[i++].setContact("flo", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//            phone_book[i++].setContact("malo", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//            phone_book[i++].setContact("mathieu", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//            phone_book[i++].setContact("jududu", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
-//        }
-        else{
-            std::cout << "Command must be one of the following : 'EXIT', 'SEARCH' or 'ADD'." << std::endl;
-        }
-        std::cin.clear();
-    }
+			std::cout << "Which contact do you want to display ? " << std::endl;
+			std::getline(std::cin, input);
+			std::stringstream ss(input);
+			if ((ss >> research_id).fail())
+			{
+				research_id = -1;
+			}
+			// std::cout << "id = " << research_id << std::endl;
+			if (research_id > 7 || research_id < 0 || !phone_book[research_id].isSet())
+			{
+				std::cout << "Invalid Id." << std::endl;
+			}
+			else
+			{
+				std::cout << "selected id : " << research_id << std::endl;
+				phone_book[research_id].display();
+			}
+		}
+		//        else if (input == "FILL"){
+		//            phone_book[i++].setContact("tititititi", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//            phone_book[i++].setContact("ratatatatatatatatatatatatatata", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//            phone_book[i++].setContact("toto", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//            phone_book[i++].setContact("MINET", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//            phone_book[i++].setContact("flo", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//            phone_book[i++].setContact("malo", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//            phone_book[i++].setContact("mathieu", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//            phone_book[i++].setContact("jududu", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t");
+		//        }
+		else
+		{
+			std::cout << "Command must be one of the following : 'EXIT', 'SEARCH' or 'ADD'." << std::endl;
+		}
+		std::cin.clear();
+	}
 
-    return 0;
+	return 0;
 }
