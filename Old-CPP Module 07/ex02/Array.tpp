b834474d1fad12e584fb50f1881bd9c6fee1231c
Sithi5 <ma.sithis@gmail.com>
2021-03-11 15:29:54 +0100

    sending ex02

--- Old-CPP Module 07/ex02/Array.tpp
+++ Old-CPP Module 07/ex02/Array.tpp
@@ -3,40 +3,58 @@
 
 #include <iostream>
 
-class IndexOutOfRange : public std::exception
-{
+class IndexOutOfRange : public std::exception {
 public:
-    virtual const char *what() const throw() {return ("Index out of range.");}
+    virtual const char *what() const throw() { return ("Index out of range."); }
 };
 
 template<typename T>
-class Array
-{
+class Array {
 private:
-	T *_array;
-	unsigned int _size;
+    T *_array;
+    unsigned int _size;
 public:
-	Array(void): _array(new T[0]),_size(0) {}
-	Array(unsigned int n): _array(new T[n]), _size(n) {}
-	Array(Array const &copy): _array(copy.getArray()) {}
-	virtual ~Array(void){}
+    Array(void) : _array(new T[0]), _size(0) {}
 
-    unsigned int size(){
+    Array(unsigned int n) : _array(new T[n]), _size(n) {}
+
+    Array(Array const &copy): _array(new T[0]), _size(0){
+        *this = copy;
+    }
+
+    Array &operator=(Array const &rhs){
+        if (this != &rhs)
+        {
+            if (this->size() > 0){
+                delete [] this->_array;
+            }
+            this->_size = rhs.size();
+            this->_array = new T[this->size()];
+            for (unsigned int i = 0; i < this->size(); i++){
+                this->_array[i] = rhs.getArray()[i];
+            }
+        }
+        return *this;
+    }
+
+    virtual ~Array(void) {
+        delete [] this->getArray();
+    }
+
+    unsigned int size() const {
         return this->_size;
     }
 
-    T * getArray(void) {return _array;}
+    T *getArray(void) const { return _array; }
 
 
-
-    T & operator[](unsigned int n) throw(std::exception) {
-		if (n >= this->size()) {
+    T &operator[](unsigned int n) throw(std::exception) {
+        if (n >= this->size()) {
             throw IndexOutOfRange();
-        }
-		else{
+        } else {
             return this->getArray()[n];
-		}
-	}
+        }
+    }
 
 
 };
--- Old-CPP Module 07/ex02/main.cpp
+++ Old-CPP Module 07/ex02/main.cpp
@@ -1,11 +1,16 @@
 #include "Array.tpp"
 
 int main() {
+
     Array<std::string> string_array(2);
     Array<std::string> *heap_string_array = new Array<std::string>(2);
 
     string_array[0] = "abcd";
     string_array[1] = "efgh";
+
+    Array<std::string> string_cpy_array(string_array);
+    string_cpy_array[0] = "has been modified";
+    string_cpy_array[1] = "has been modified 2";
 
     heap_string_array[0][0] = "ijkl";
     heap_string_array[0][1] = "mnop";
@@ -26,6 +31,7 @@ int main() {
         std::cerr << e.what() << std::endl;
     }
 
+//    Empty array
     Array<std::string> string_array_empty;
 
     try{
@@ -33,6 +39,65 @@ int main() {
     }
     catch (std::exception const &e) {
         std::cerr << "An error occured : " << e.what() << std::endl;
+    }
+
+
+    std::cout <<"TESTING CPY AND ASSIGNATION WITH INT ARRAY"  << std::endl;
+//    int array
+    Array<int> int_array(2);
+    int_array[0] = 0;
+    int_array[1] = 5;
+
+    Array<int> cpy_int_array(int_array);
+    Array<int> cpy2_int_array(2);
+    Array<int> cpy3_int_array(3);
+    cpy3_int_array[0] = -456;
+    cpy3_int_array[1] = 4242;
+    cpy3_int_array[2] = 7;
+    cpy2_int_array = cpy_int_array;
+    try{
+        std::cout << "cpy3_int_array content : " << std::endl;
+        for (unsigned int i = 0; i < cpy3_int_array.size(); i++)
+            std::cout << "list[" << i << "] = " << cpy3_int_array[i] << std::endl;
+        cpy3_int_array = cpy2_int_array;
+    }
+    catch (std::exception const &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    try {
+        std::cout << "int_array size : " << int_array.size() << std::endl;
+        std::cout << "cpy_int_array size : " << cpy_int_array.size() << std::endl;
+        std::cout << "cpy2_int_array size : " << cpy2_int_array.size() << std::endl;
+
+        std::cout << "int_array content : " << std::endl;
+        for (unsigned int i = 0; i < int_array.size(); i++)
+            std::cout << "list[" << i << "] = " << int_array[i] << std::endl;
+
+        std::cout << "cpy_int_array content : " << std::endl;
+        for (unsigned int i = 0; i < cpy_int_array.size(); i++)
+            std::cout << "list[" << i << "] = " << cpy_int_array[i] << std::endl;
+
+        std::cout << "cpy2_int_array content : " << std::endl;
+        for (unsigned int i = 0; i < cpy2_int_array.size(); i++)
+            std::cout << "list[" << i << "] = " << cpy2_int_array[i] << std::endl;
+
+        std::cout << "cpy3_int_array content : " << std::endl;
+        for (unsigned int i = 0; i < cpy3_int_array.size(); i++)
+            std::cout << "list[" << i << "] = " << cpy3_int_array[i] << std::endl;
+
+
+//        Testing if updating a value of a cpy will change value of original
+        cpy2_int_array[0] = 999;
+        cpy2_int_array[1] = -456;
+        std::cout << "cpy2_int_array content : " << std::endl;
+        for (unsigned int i = 0; i < cpy2_int_array.size(); i++)
+            std::cout << "list[" << i << "] = " << cpy2_int_array[i] << std::endl;
+        std::cout << "int_array content : " << std::endl;
+        for (unsigned int i = 0; i < int_array.size(); i++)
+            std::cout << "list[" << i << "] = " << int_array[i] << std::endl;
+    }
+    catch (std::exception const &e) {
+        std::cerr << e.what() << std::endl;
     }
 
     return 0;
