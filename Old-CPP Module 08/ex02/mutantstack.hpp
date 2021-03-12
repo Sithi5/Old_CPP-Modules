78d64d624b8abac1a42d1353def57da644add1e3
Florian Blanchard <florian5611@gmail.com>
2021-03-12 16:25:03 +0100

    du mieux mais compile pas

--- Old-CPP Module 08/ex02/mutantstack.hpp
+++ Old-CPP Module 08/ex02/mutantstack.hpp
@@ -12,13 +12,14 @@ public:
     MutantStack(void);
     MutantStack(MutantStack const &copy);
     virtual ~MutantStack(void);
-    MutantStack &operator=(MutantStack const &rhs);
+    MutantStack<T> &operator=(MutantStack<T> const &rhs);
 
-    typedef typename std::stack<Tr>::container_type::iterator iterator;
-    iterator begin(void) { return std::stack<T>::c.begin(); }
-    iterator rbegin(void);
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    iterator begin(void);
+    reverse_iterator rbegin(void);
     iterator end(void);
-    iterator rend(void);
+    reverse_iterator rend(void);
 };
 
 #endif
